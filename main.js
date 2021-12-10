af9fd1372c6554d373bb24b0e82a7222e8191777
unknown <kls1713@mail.hongik.ac.kr>
2021-12-10 18:36:51 +0900

    login

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 80;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+const qs = require('querystring');
 const fs = require('fs');
+const cookie = require('cookie');
 
 app.set('port', port);
 
@@ -12,31 +13,56 @@ app.use(express.static('public'));
 app.use(express.static('node_modules/socket.io/client-dist'));
 
 
+app.post('/login_process', (req, res)=>{
+    let body = '';
+    req.on('data', (data)=>{
+        body += data;
+    });
+    req.on('end', ()=>{
+        let post = qs.parse(body);
+        let username = post.name;
+        let servername = post.serverName;
+
+        res.writeHead(302, {
+            'Set-Cookie':[
+                `name=${username}`,
+                `server=${servername}`
+            ],
+            Location: `/`
+        });
+        res.end();
+    })
+});
+
+
+app.get('/bounce', (req,res)=>{
+    res.sendFile(__dirname + '/views/bounceball.html');
+});
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html')
-})
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/chat', (req, res)=>{
+    res.sendFile(__dirname + '/views/chat.html');
+});
 app.get('/', (req, res, next)=>{
-    res.sendFile(__dirname + '/views/chat.html')
+    let cookies = {};
+    if(req.headers.cookie !== undefined){
+        cookies = cookie.parse(req.headers.cookie);
+        res.writeHead(302, {user: cookies.name, server: cookies.server ,Location: `/chat`});
+    } else{
+        res.writeHead(302, {Location: `/login`});
+    }
+    res.end();
 })
-
-
-
-
-/* view engine set to load html file
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
-*/
 
 //접속중인 유저
 let user = [];
 
 io.on('connection', (socket)=>{
     console.log(`${socket.id} conncected`);
-    user.push(socket.id);
 
     socket.on('disconnect', ()=>{
         io.emit('exit', `${socket.id}`);
-        
         
         //userlist 송출
         user.splice(user.indexOf(socket.id), 1);
@@ -44,12 +70,14 @@ io.on('connection', (socket)=>{
     })
 
     socket.on('join', (id)=>{
+        console.log(`${id} conncected`);
         socket.broadcast.emit('join', id);
-
+        user.push({socketId: socket.id, name: id});
         io.emit('userlist', user);
     })
     socket.on('message', (data)=>{
         socket.broadcast.emit('message', {
+            socketId: data.socketId,
             name: data.name,
             message: data.message,
             time : data.time            
@@ -58,5 +86,5 @@ io.on('connection', (socket)=>{
 })
 
 http.listen(port, ()=>{
-    console.log('server online');
+    console.log(`server online on port #${port}`);
 })
--- public/js/chat.js
+++ public/js/chat.js
@@ -45,6 +45,18 @@ function addMyChat(content){
     $target.scrollTop = $target.scrollHeight;
 }
 
+function addNotice(content){
+    let $target = document.getElementById("content");
+    let message = `
+        <div class="notice">
+            <p class="my-content-message">${content}</p>
+        </div>  
+    `
+    $target.innerHTML += message;
+    $target.scrollTop = $target.scrollHeight;    
+}
+
+
 let list = [1,2,3,4];
 function setUserlist(list){
     if(list.length < 1) return;
@@ -62,7 +74,16 @@ function setUserlist(list){
     $userCount.innerHTML = list.length;
 }
 
-
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 
 window.onload = ()=>{
     const $leftDrawerButton = document.querySelector('#menu-list').firstElementChild;
